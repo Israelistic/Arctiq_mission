c885836bcd6bd5e8034508da9cca113765d25668
Haggai Lerman <haguyle@gmail.com>
2019-10-06 22:14:45 -0400

    feat:  redesigned the infrastracture to terraform
    In order to accomedate web-application deployment I had to change the
    infrasracture design. I added a new logic to the naming of the regions
    and provided tags to the subnet. Intreduced maping of variable
    
    ver: 1.2

--- terraform_plans/variables.tf
+++ terraform_plans/variables.tf
@@ -1,1 +1,22 @@
+variable "aws_profile" {}
+variable "region_antartica" {}
+variable "region_north_pole" {}
+
+variable "vpc_antartica_cider" {}
+variable "vpc_north_pole_cider" {}
+
+variable "cidrs_antartica" {
+  type = "map"
+}
+variable "cidrs_north_pole" {
+  type = "map"
+}
+
+
+data "aws_availability_zones" "available" {}
+
+variable "canada_az" {
+  type = "map"
+}
+
 
