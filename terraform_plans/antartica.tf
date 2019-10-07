815e811a5a7de101bbf44da2ccd83d4117809447
Haggai Lerman <haguyle@gmail.com>
2019-10-07 04:37:16 -0400

    feat: added RDS instances
    configured RDS instances for antartica and northpole

--- terraform_plans/antartica.tf
+++ terraform_plans/antartica.tf
@@ -522,6 +522,7 @@ resource "aws_security_group" "antartica_RDS_sg" {
   description = "Used for RDS instances"
   vpc_id      = "${aws_vpc.antartica_vpc.id}"
   #SQL access from public/private security group
+
   ingress {
     from_port       = 3306
     to_port         = 3306
@@ -536,6 +537,7 @@ resource "aws_security_group" "north_pole_RDS_sg" {
   description = "Used for RDS instances"
   vpc_id      = "${aws_vpc.north_pole_vpc.id}"
   #SQL access from public/private security group
+
   ingress {
     from_port       = 3306
     to_port         = 3306
@@ -608,4 +610,30 @@ resource "aws_s3_bucket" "north_pole_code" {
   tags = {
     Name = "north_pole_code_bucket"
   }
+}
+######################RDS instances####################
+resource "aws_db_instance" "antartica_wp_db" {
+  allocated_storage      = 10
+  engine                 = "mysql"
+  engine_version         = "5.6.34"
+  instance_class         = "${var.db_instance_class}"
+  name                   = "${var.dbname1}"
+  username               = "${var.dbuser}"
+  password               = "${var.dbpassword}"
+  db_subnet_group_name   = "${aws_db_subnet_group.antartica_rds_subnet_grp.name}"
+  vpc_security_group_ids = ["${aws_security_group.antartica_RDS_sg.id}"]
+  skip_final_snapshot    = true
+}
+resource "aws_db_instance" "north_pole_wp_db" {
+  provider               = aws.central
+  allocated_storage      = 10
+  engine                 = "mysql"
+  engine_version         = "5.6.34"
+  instance_class         = "${var.db_instance_class}"
+  name                   = "${var.dbname2}"
+  username               = "${var.dbuser}"
+  password               = "${var.dbpassword}"
+  db_subnet_group_name   = "${aws_db_subnet_group.north_pole_rds_subnet_grp.name}"
+  vpc_security_group_ids = ["${aws_security_group.north_pole_RDS_sg.id}"]
+  skip_final_snapshot    = true
 }
--- terraform_plans/variables.tf
+++ terraform_plans/variables.tf
@@ -1,15 +1,16 @@
+variable "db_instance_class" {}
+variable "dbname1" {}
+variable "dbname2" {}
+variable "dbuser" {}
+variable "dbpassword" {}
 
 variable "local_ip" {}
-variable "domain_name" {
-}
-
+variable "domain_name" {}
 variable "aws_profile" {}
 variable "region_antartica" {}
 variable "region_north_pole" {}
-
 variable "vpc_antartica_cider" {}
 variable "vpc_north_pole_cider" {}
-
 variable "cidrs_antartica" {
   type = "map"
 }
