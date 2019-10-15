e041687574eec7a9ee0c5d6bf9b85cc06a97a475
Haggai Lerman <haguyle@gmail.com>
2019-10-15 11:41:49 -0400

    fix: fixed the dns recorods to root level

--- terraform_plans/route53.tf
+++ terraform_plans/route53.tf
@@ -1,14 +1,14 @@
 
 # Primary Zone
 resource "aws_route53_zone" "antartica_primary" {
-  name              = "${var.domain_name}.com"
+  name              = "${var.domain_name}.ca"
   delegation_set_id = "${var.delegation_set}"
 }
 
 #WWW
 resource "aws_route53_record" "www" {
   zone_id = "${aws_route53_zone.antartica_primary.zone_id}"
-  name    = "www.${var.domain_name}.com"
+  name    = "www.${var.domain_name}.ca"
   type    = "A"
 
   alias { # the alias is point to the load blancer. every time elb changes his ip the alias will help it to follow the address
@@ -21,7 +21,7 @@ resource "aws_route53_record" "www" {
 # Dev
 resource "aws_route53_record" "antartica_record_dev" {
   zone_id = "${aws_route53_zone.antartica_primary.zone_id}"
-  name    = "dev.${var.domain_name}.com"
+  name    = "dev.${var.domain_name}.ca"
   type    = "A"
   ttl     = "300"
   records = ["${aws_instance.antartica_penguin_wp_dev.public_ip}"]
@@ -29,20 +29,17 @@ resource "aws_route53_record" "antartica_record_dev" {
 # Private Zone
 
 resource "aws_route53_zone" "antartica_secondary_record" {
-  name = "${var.domain_name}.com"
-  vpc {
-    vpc_id = "${aws_vpc.antartica_vpc.id}"
-  }
+  name = "${var.domain_name}.ca"
+  vpc_id = "${aws_vpc.antartica_vpc.id}"
 }
 
 #BD
 resource "aws_route53_record" "antartica_db_record" {
   zone_id = "${aws_route53_zone.antartica_secondary_record.zone_id}"
-  name    = "db.${var.domain_name}.com"
-  type    = "CNAME"
-  ttl     = "300"
-  records = ["${aws_db_instyes
-  ance.antartica_wp_db.address}"]
+  name = "db.${var.domain_name}.ca"
+  type = "CNAME"
+  ttl = "300"
+  records = ["${aws_db_instance.antartica_wp_db.address}"]
 }
 
 
